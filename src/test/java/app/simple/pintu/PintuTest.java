aac6d9fe9c7501f64b1f0a0cc01d0ed81115d5ae
wisesa15 <priyo.tw@gmail.com>
2022-03-27 15:53:01 +0700

    Menambahkan kelas unit test untuk kelas Pintu dan kelas Thermometer

---
+++ src/test/java/app/simple/pintu/PintuTest.java
@@ -0,0 +1,14 @@
+package app.simple.pintu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PintuTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/app/simple/thermometer/ThermometerTest.java
@@ -0,0 +1,14 @@
+package app.simple.thermometer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ThermometerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
